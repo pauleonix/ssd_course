de9aafd1c5c9e79f7565ba1071542e09f9f4d21e
Bruno Gideon Bergheim <bergheim@stud.uni-heidelberg.de>
2022-03-04 12:39:24 +0100

    Adds optional csv read keywords

--- src/input_output.py
+++ src/input_output.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
-def read_table(fname: str, sep: str = r"(?<!\()\s\s+") -> pd.DataFrame:
+def read_table(
+    fname: str, sep: str = r"(?<!\()\s\s+", csv_read_kwargs={}
+) -> pd.DataFrame:
     """
     Reads the content of a data file as pandas DataFrame.
 
@@ -11,12 +13,14 @@ def read_table(fname: str, sep: str = r"(?<!\()\s\s+") -> pd.DataFrame:
     Returns:
     pandas.DataFrame
     """
-    if fname.endswith(".dat"):
+    if fname.endswith(".dat"):  # special case for the dat file wich jas i an j as index
         idx_col = [0, 1]
     else:
         idx_col = 0
 
-    return pd.read_csv(fname, index_col=idx_col, sep=sep, engine="python")
+    return pd.read_csv(
+        fname, index_col=idx_col, sep=sep, engine="python", **csv_read_kwargs
+    )
 
 
 if __name__ == "__main__":
