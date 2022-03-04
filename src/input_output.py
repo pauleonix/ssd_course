f81a00f1250ea47be1cd4c02d1413f799d5e712e
Bruno Gideon Bergheim <bergheim@stud.uni-heidelberg.de>
2022-03-04 13:24:45 +0100

    Improves documentation for read table

--- src/input_output.py
+++ src/input_output.py
@@ -2,18 +2,21 @@ import pandas as pd
 
 
 def read_table(
-    fname: str, sep: str = r"(?<!\()\s\s+", csv_read_kwargs={}
+    fname: str, sep: str = r"(?<!\()\s\s+", csv_read_kwargs: dict = {}
 ) -> pd.DataFrame:
     """
     Reads the content of a data file as pandas DataFrame.
 
-    fname -> str: path to file
-    sep -> str: default: r"(?<!\()\s\s+"  Regex-pattern marking the delimer of the file.
+    fname: str -> path to file
+    sep: str ->  default: r"(?<!\()\s\s+"  Regex-pattern marking the
+                delimer of the file.
+    csv_read_kwargs dict: -> Dictionary of key word arguments that is
+                passed to pandas.read_csv()
 
     Returns:
     pandas.DataFrame
     """
-    if fname.endswith(".dat"):  # special case for the dat file wich jas i an j as index
+    if fname.endswith(".dat"):  # special case for the dat file
         idx_col = [0, 1]
     else:
         idx_col = 0
@@ -24,5 +27,4 @@ def read_table(
 
 
 if __name__ == "__main__":
-    x = read_table("data/table.dat")
-    print(x.shape)
+    pass
