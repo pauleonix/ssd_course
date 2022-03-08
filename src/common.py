1ffb9c7622bef15e59473e521ea69c54ca008754
B.G.Bergheim <b.bergheim@gmx.de>
2022-03-08 22:21:20 +0100

    Improves Documentation for common module

--- src/common.py
+++ src/common.py
@@ -5,7 +5,16 @@ import pandas as pd
 
 
 def drop_constants(data: pd.DataFrame, threshold: float = 1e-4):
-    """ """
+    """Takes a pandas Dataframe and drops all columns that have a varaince that
+    is lower than a given threshold.
+
+    Args:
+        data: pandas Dataframe containingthe dataframe.
+        theshold: float (default = 1e-4) ariance threshold.
+    Returns:
+        pandas Dataframe
+
+    """
     const_column_name = [
         label for label, content in data.items() if content.var() < threshold
     ]
