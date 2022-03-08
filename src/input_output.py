6d6b80197b4da76b69b3f7a466166f3f5894c193
B.G.Bergheim <b.bergheim@gmx.de>
2022-03-08 13:09:41 +0100

    Changes Doc strings to follow Google guides

--- src/input_output.py
+++ src/input_output.py
@@ -8,16 +8,17 @@ def read_table(
 ) -> pd.DataFrame:
     """Reads the content of a data file as pandas DataFrame.
 
-    fname: str -> path to file
+    Args:
+        fname: str -> path to file
 
-    sep: str ->  default: r"(?<!\()\s\s+"  Regex-pattern marking the delimer of
-    the file.
+        sep: str ->  default: r"(?<!\\()\s\s+"  Regex-pattern marking the delimiter of
+        the file.
 
-    csv_read_kwargs dict: -> Dictionary of key word arguments that is passed to
-    pandas.read_csv()
+        csv_read_kwargs: dict -> Dictionary of key word arguments that is passed to
+        pandas.read_csv()
 
     Returns:
-    pandas.DataFrame
+        pandas.DataFrame
     """
 
     return pd.read_csv(fname, header=0, sep=sep, engine="python", **csv_read_kwargs)
@@ -26,18 +27,21 @@ def read_table(
 def read_table_as_numpy(
     fname: str, sep: str = r"(?<!\()\s\s+", pandas_read_kwargs={}
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Reads a data table as pandas DataFrame and then return a tuple the
-    values of the frame as c-contigious numpy array.
+    """Reads a data table as pandas DataFrame and then returns a tuple which contains:
+     the values of the frame the index as numpy array and the column names as
+     c-contigious numpy array.
 
+    Args:
+        fname: str -> path to file
 
-    fname: str -> path to file
-    sep: str ->  default: r"(?<!\()\s\s+"  Regex-pattern marking the delimer of
-    the file.
-    pandas_read_kwargs dict: -> Dictionary of key word arguments that is
-    passed to pandas.read_csv()
+        sep: str ->  default: r"(?<!\\()\s\s+"  Regex-pattern marking the delimiter of
+        the file.
+
+        pandas_read_kwargs: dict -> Dictionary of key word arguments that is
+        passed to pandas.read_csv()
 
     Returns:
-    values as numpy.ndarray, index as numpy.ndarray, columns as numpy.ndarray
+        Tuple(Values as numpy.ndarray, index as numpy.ndarray, columns as numpy.ndarray)
     """
     df = read_table(fname, sep, csv_read_kwargs=pandas_read_kwargs)
     i_cols = "time"
